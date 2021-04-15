cd5c0d590e013bee0172563daf29b7e36dad8c30
iamkylert <kyletechentin20@gmail.com>
2021-04-15 18:21:40 -0400

    added the rest of the GUIs needed. But need to find a way to change the GUI contents easily by checking conditions of a static variable maybe.

--- src/GameDisplayForm.java
+++ src/GameDisplayForm.java
@@ -4,35 +4,41 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GameDisplayForm {
-    private JPanel panel1;
     private JButton option2Button;
     private JButton option1Button;
+    public JPanel mainDisplay;
+    public static JFrame myFrame;
+
 
     public GameDisplayForm() {
         option1Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                myFrame = new JFrame("Option 1");
+                myFrame.setContentPane(new OptionInterfaceForm().optionFeedback);
+                myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                myFrame.setPreferredSize(new Dimension(1000, 600));
+                myFrame.pack();
+                myFrame.setVisible(true);
 
             }
         });
         option2Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                myFrame = new JFrame("Option 2");
+                myFrame.setContentPane(new OptionInterfaceForm().optionFeedback);
+                myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                myFrame.setPreferredSize(new Dimension(1000, 600));
+                myFrame.pack();
+                myFrame.setVisible(true);
 
             }
         });
     }
+    public void optionInterface(){
 
-    public static void main(String[] args) {
-        JFrame myFrame = new JFrame ("This is a Generalized Anxiety Training Simulation"); // Makes the frame
-        myFrame.setContentPane(new GameDisplayForm().panel1); // attaches the panel to the frame
-
-        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        myFrame.setPreferredSize(new Dimension(1000,600));
-        myFrame.setResizable(true);
-
-        myFrame.pack(); // puts all components into the frame
-        myFrame.setVisible(true); // allows the frame to be seen
     }
+
+
 }
--- src/IntroPageForm.java
+++ src/IntroPageForm.java
@@ -1,5 +1,36 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class IntroPageForm {
-    private JPanel panel1;
+    public JPanel welcomeForm;
+    private JButton clickToStartButton;
+    public static JFrame myFrame;
+
+    public IntroPageForm() {
+        clickToStartButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                myFrame.setContentPane(new GameDisplayForm().mainDisplay);
+                myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                myFrame.setPreferredSize(new Dimension(1000, 600));
+                myFrame.pack();
+                myFrame.setVisible(true);
+
+            }
+        });
+    }
+    public static void main(String[] args) {
+        JFrame myFrame = new JFrame ("This is a Generalized Anxiety Training Simulation"); // Makes the frame
+        myFrame.setContentPane(new IntroPageForm().welcomeForm); // attaches the panel to the frame
+
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        myFrame.setPreferredSize(new Dimension(1000,600));
+        myFrame.setResizable(true);
+
+        myFrame.pack(); // puts all components into the frame
+        myFrame.setVisible(true); // allows the frame to be seen
+    }
 }
