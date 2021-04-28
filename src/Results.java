3dfdf77b2368faa076e2243f42c1c630cf94b201
hfagg12 <h-fagg@onu.edu>
2021-04-28 15:36:30 -0400

    Added the results GUI

---
+++ src/Results.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Results {
+    private JPanel panel1;
+    private JLabel scoreLabel;
+    private JTextField textField;
+
+    public void nonEditField(){
+        textField.setEditable(false);
+
+    }
+}
