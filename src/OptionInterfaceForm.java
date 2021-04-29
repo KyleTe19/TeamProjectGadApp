45ff1b624b24a8dac71c9202d77e2e7594cb1a8a
iamkylert <kyletechentin20@gmail.com>
2021-04-28 21:06:35 -0400

    Grammar fixes

--- src/OptionInterfaceForm.java
+++ src/OptionInterfaceForm.java
@@ -41,39 +41,67 @@ public class OptionInterfaceForm {
         {
             switch (scenario){
                 case 1:
-                    textArea1.setText("It is important to recognize mental health issues even in individuals who you are not very close with. If unrecognized by anyone, mental health conditions can become harder to gain control of. One of the symptoms of many mental health conditions is lack of sleep which induces tiredness. This then will lead to having difficulty concentrating/ seeming spaced out. ");
+                    textArea1.setText("It is important to recognize mental health conditions even in individuals whom" +
+                            " you are not very close with. If unrecognized, mental health conditions can become harder to gain control of. " +
+                            "One of the symptoms of many mental health conditions is lack of sleep which induces tiredness. " +
+                            "This then will lead to having difficulty concentrating/ spacing out. ");
                     textArea2.setText("One thing to keep in mind is that anxiety is very common among adolescents and" +
                             " children. 7.1% of children aged 3-17 years (approximately 4.4 million) have diagnosed " +
-                            "anxiety. Its a very normal thing which is why education on it are so important! ");
+                            "anxiety. Its a very normal thing which is why education on it is so important! ");
                     break;
                 case 2:
-                    textArea1.setText("Being on edge and restless is an indication for multiple different mental health disorders. It is best to not assume that someone is okay based on only your experience with other individuals who have difficulty with certain projects at school. ");
+                    textArea1.setText("Being on edge and restless is an indication of multiple different mental " +
+                            "health disorders. It is best not to assume that someone is okay based on preconceptions." +
+                            " Plus it is always better to be safe than!" );
+
                     textArea2.setText("");
                     break;
                 case 3:
-                    textArea1.setText("Excessive worry and family problems can increase mental health conditions. Generalized Anxiety Disorder develops slowly and often starts in the teen years. Both children and adults with GAD may experience physical symptoms that make it hard to function and that interfere with daily life. So, if this mental health condition were to continue without any recognition, then it would be more difficult to help as a mental health first aider.");
-                    textArea2.setText("A website with more information: https://www.nimh.nih" +
-                            ".gov/health/publications/generalized-anxiety-disorder-gad/index.shtml");
+                    textArea1.setText("Excessive worry and family problems can be a catalyst for  mental health " +
+                            "conditions. Generalized Anxiety Disorder develops slowly and often starts in the teen years. " +
+                            "Both children and adults with GAD may experience physical symptoms that make it hard to function" +
+                            " causing heavy interference with daily life. So, if this mental health condition were to" +
+                            " continue without any recognition, it would become increasingly difficult to help as a " +
+                            "mental health first aider.");
+
+                    textArea2.setText("");
                     break;
             }
         }
         if (option == 2) {
             switch (scenario) {
                 case 1:
-                    textArea1.setText("Good choice! You were able to understand that something was wrong with your classmate and decided to talk to them despite not having a very close friendship. The first step as a mental health first aider is identifying a potential problem in an individual. Be sure that you are prepared to listen because oftentimes the best thing you can do is just be someone that the affected person can talk to. ");
+                    textArea1.setText("Good choice! You were able to understand that your classmate could be dealing " +
+                            " with more than just a bad day" +
+                            " and decided to talk to them despite not having a very close friendship. The first step" +
+                            " as a mental health first aider is being able to recognize signs of a condition in " +
+                            "others. Be sure that you are prepared to listen because oftentimes the best thing you can " +
+                            "do is just be someone that the affected person can talk to.");
+
                     textArea2.setText("One thing to keep in mind is that anxiety is very common among adolescents and" +
                             " children. 7.1% of children aged 3-17 years (approximately 4.4 million) have diagnosed " +
-                            " anxiety. Its a very normal thing which is why education on it are so important!");
+                            " anxiety. Its a very normal thing which is why education on it is so important!");
 
                     break;
+
                 case 2:
-                    textArea1.setText("Good Job! You recognized that the individual was possibly being affected by anxiety. He showed signs of restlessness and being on edge, which can be major signs of anxiety. Whether he accepts your invitation to sit with him or not, he knows that you are concerned. This might open up more opportunities to share.");
+                    textArea1.setText("Good Job! You recognized that the individual was possibly being affected by anxiety. " +
+                            "He showed signs of restlessness and being on edge, which can be major signs of anxiety. " +
+                            "Whether he accepts your invitation to sit with him or not does not really matter, he " +
+                            "knows that you are " +
+                            "concerned and this might open up more opportunities to help them later.");
+
                     textArea2.setText("");
+
                     break;
+
                 case 3:
-                    textArea1.setText("Good job! You recognized that it was important to not attempt to fix the individual’s problems. You understood that letting the individual know that you are available to listen to their problems was the best that you could do under these circumstances. It is important to be sensitive to what an individual with anxiety is going through, even if it doesn’t make sense to you. Be open for when he reaches out to you again in the future.");
-                    textArea2.setText("A website with more information: https://www.nimh.nih" +
-                            ".gov/health/publications/generalized-anxiety-disorder-gad/index.shtml");
+                    textArea1.setText("Good job! You recognized that it was important to not attempt to fix the individual’s problems. " +
+                            "You understood that letting the individual know that you are available to listen to their problems " +
+                            "was the best that you could do under these circumstances. It is important to be sensitive to " +
+                            "what an individual with anxiety is going through, even if it does not make sense to you." +
+                            "Be open for when they reach out to you again.");
+                    textArea2.setText("");
                     break;
             }
         }
